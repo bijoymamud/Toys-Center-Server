c7df98ec68b09871325f4a59032970f4b037399a
bijoymamud <bijoymamud.09@gmail.com>
2023-05-20 10:59:43 +0600

    added single toy info with id

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -64,7 +64,13 @@ async function run() {
 
 
 
+    app.get("/toyinfo/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyInformationCollection.findOne(query);
+      res.send(result);
 
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
